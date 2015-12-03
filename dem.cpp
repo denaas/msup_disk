d438c90bc2d6428d21e37cf4e14b0feea939e40b
KolyambaDoc <nik-demix@mail.ru>
2015-12-03 22:59:10 +0300

    make classes,make plugs and work with socket

--- dem.cpp
+++ dem.cpp
@@ -8,13 +8,7 @@
 #include <string.h>
 #include <iostream>
 #include <netinet/in.h> // For OS X and Linux. Doesn't need for Free BSD
-
-struct info_struct {
-	int key;
-    int ls,fd;
-	int x;
-	fd_set readfds;	
-};
+#include "header.h"
 
 void reverse(char s[])
 {
@@ -118,8 +112,17 @@ int word_length(char *s)
 	{}
 	return i;
 }
+void ACTION::makemasterkey(){} //смотри хэдер ,класс USB
+char* ACTION::shifrovat(char *adr){}//задается адрес для шифрования
+void ACTION::in_storage(char *str){}//вставляем шимфр текст в виртуальную память
+void ACTION::del_disk(char *adr){} //удаление файлов из диска
+char* ACTION::from_storage(char *adr){}//берет из виртуалки конкретный файл
+char* ACTION::rasshifrovat(char *adr){}//расшифровывает файл(что выдает пока непонятно)
+void ACTION::makefile(char*str){}//создает файл с содержанием стр
+void ACTION::delete_storage(){}//удаляет виртуальную память
+void ACTION::open_text(char*str){} //открывает во втором клиенте результирующий файл, возможно создает файл, клиент его открывает выводит, а потом удаляет
 
-void do_delete(struct info_struct *b)
+void ACTION::do_delete(struct info_struct *b)
 {
     int i = 0;
     char * str = new char[100];
@@ -142,12 +145,31 @@ void do_delete(struct info_struct *b)
     std::cout<<str<<std::endl;
     std::cout<<log<<std::endl;
     std::cout<<pin<<std::endl;
-     //код для работы функции здесь, если ошибка возвращать не окей
-    strcpy(str,"Okey\0");
-    write(b->fd,str,strlen(str)+1);
+    int flag = 0;
+    flag = count_USB();
+    if (flag == 1) {
+        takeusbinf();
+        token.log = log;
+        token.pin = pin;
+        makemasterkey();
+        char *res;
+        //for(по всем файла из папки - str)
+        {
+            res = from_storage(res); //внутри функции должен быть адрес конкретного файла
+            res = rasshifrovat(res);
+            makefile(res);           //создает файл с содержанием res  в папке str
+        }
+        delete_storage();       //удаляет виртуальную память
+        strcpy(str,"Okey\0");
+        write(b->fd,str,strlen(str)+1);
+        }
+    else{
+        strcpy(str,"Mistake\0");
+        write(b->fd,str,strlen(str)+1);
+    }
 }
 
-void do_encode(struct info_struct *b)
+void ACTION::do_encode(struct info_struct *b)
 {
     int i = 0;
     char * str = new char[100];
@@ -170,12 +192,26 @@ void do_encode(struct info_struct *b)
     std::cout<<str<<std::endl;
     std::cout<<log<<std::endl;
     std::cout<<pin<<std::endl;
-     //код для работы функции здесь, если ошибка возвращать не окей
-    strcpy(str,"Okey\0");
-    write(b->fd,str,strlen(str)+1);
+    int flag = 0;
+    flag = count_USB();
+    if (flag == 1) {
+        takeusbinf();
+        token.log = log;
+        token.pin = pin;
+        makemasterkey();
+        char* res = shifrovat(str);
+        in_storage(res);
+        del_disk(str);
+        strcpy(str,"Okey\0");
+        write(b->fd,str,strlen(str)+1);
+        }
+    else{
+        strcpy(str,"Mistake\0");
+        write(b->fd,str,strlen(str)+1);
+    }
 }
 
-void do_decode(struct info_struct *b)
+void ACTION::do_decode(struct info_struct *b)
 {
     int i = 0;
     char * str = new char[100];
@@ -198,37 +234,48 @@ void do_decode(struct info_struct *b)
     std::cout<<str<<std::endl;
     std::cout<<log<<std::endl;
     std::cout<<pin<<std::endl;
-     //код для работы функции здесь, если ошибка возвращать не окей
-    strcpy(str,"Okey\0");
-    write(b->fd,str,strlen(str)+1);
+    int flag = 0;
+    flag = count_USB();
+    if (flag == 1) {
+        takeusbinf();
+        token.log = log;
+        token.pin = pin;
+        makemasterkey();
+        char *res;
+        res = from_storage(res); //внутри функции должен быть адрес конкретного файла
+        res = rasshifrovat(res);
+        open_text(res); //открывает во втором клиенте результирующий файл, возможно создает файл, клиент его открывает выводит, а потом удаляет
+        strcpy(str,"Okey\0");
+        write(b->fd,str,strlen(str)+1);
+        }
+    else{
+        strcpy(str,"Mistake\0");
+        write(b->fd,str,strlen(str)+1);
+    }
 }
 
-void do_alert()
+void ACTION::do_alert()
 {
 	const char buf1[] = "USB was removed!\n>";
 	printf(buf1);
 }
 
 
-void do_command(struct info_struct *b, char * cmd)
+void ACTION::do_command(struct info_struct *b, char * cmd)
 {
 	if(!strcmp(cmd,"\0")) {
 		return;
 	}
-    if(!strcmp(cmd,"delete")) {
+    if(!strcmp(cmd,"delete\0")) {
         do_delete(b);
 		return;
 	}
-	if(!strcmp(cmd,"encode")) {
+    if(!strcmp(cmd,"encode\0")) {
         do_encode(b);
 		return;
 	}
-	if(!strcmp(cmd,"alert")) {
+    if(!strcmp(cmd,"alert\0")) {
 		do_alert();
-		return;
-	}
-	if(!strcmp(cmd,"decode")) {
-        do_decode(b);
 		return;
 	}
 }
@@ -278,9 +325,10 @@ int main(int argc,char **argv)
 	struct info_struct all_info;
     struct sockaddr_in addr;
     int fd,i;
+    ACTION TrueCrypt;
     char  *str = new char[10];
     socklen_t alen;
-	int max1 = 1;
+    int max1 = 2;
 	printf("Server is ready. Maximum number of sockets is %d\n",max1);
     all_info.ls = start_listen(atoi(argv[2]));
     //before_start(&all_info);
@@ -294,7 +342,19 @@ int main(int argc,char **argv)
         if (read(fd, str+i, 1) == 0) printf("read error\n");
         }
         while(str[i++] != '\0');
-        do_command(&all_info,str);
+        if(!strcmp(str,"client_1\0")) {
+            int i = 0;
+            do {
+            if (read(fd, str+i, 1) == 0) printf("read error\n");
+            }
+            while(str[i++] != '\0');
+            ACTION client(1);
+            client.do_command(&all_info,str);
+        }
+        if(!strcmp(str,"client_2\0")) {
+            ACTION client(2);
+            client.do_decode(&all_info);
+        }
         if (read(fd, str, 1) == 0) close(fd);
 
 	}
---
+++ header.h
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <vector>
+
+class USB {
+public:
+    char *label;
+    char *UID;
+    char *log;
+    char *pin;
+    USB(){
+        label = new char[20];
+        UID = new char[20];
+        log = new char[50];
+        pin = new char[50];
+    }
+    USB (const USB &a){
+        label = new char[20];
+        UID = new char[20];
+        log = new char[50];
+        pin = new char[50];
+        strcpy(label,a.label);
+        strcpy(UID,a.UID);
+        strcpy(log,a.log);
+        strcpy(pin,a.pin);
+    }
+    ~USB(){
+        delete []label;
+        delete []log;
+        delete []UID;
+        delete []pin;
+    }
+};
+
+struct info_struct {
+    int key;
+    int ls,fd;
+    int x;
+    fd_set readfds;
+};
+
+class ACTION{
+    static int N_GETUNLIM;
+    int number;
+public:
+    USB token;
+    ACTION (int i = 0) {number = i;}
+    char* getlineunlim();
+    int count_USB();//количество флешек вставленно
+    void takeusbinf();//получаем информацию о флешке
+    void do_command(struct info_struct *b, char * cmd);//выбирает кнопку из client1
+    void do_encode(struct info_struct *b);//кнопка code - client1
+    void do_decode(struct info_struct *b);//кнопка read - client2
+    void do_delete(struct info_struct *b);//кнопка delete - client1
+    void do_alert();//останов , но не факт что будет
+    void makemasterkey();//создает мастер ключ
+    char * shifrovat(char *adr);//шифрует диск
+    void in_storage(char*str);//вставляем шимфр текст в виртуальную память
+    void del_disk(char *adr);//удаляет все файлы по адресу, чтоб на компе хранился только зашифрованный текст
+    char* from_storage(char *adr);//берет из виртуалки конкретный файл
+    char* rasshifrovat(char *adr);//расшифровывает файл(что выдает пока непонятно)
+    void makefile(char*str);//создает файл с содержанием стр
+    void delete_storage();//удаляет виртуальную память
+    void open_text(char*str);//открывает во втором клиенте результирующий файл, возможно создает файл, клиент его открывает выводит, а потом удаляет
+};
+
+
+int ACTION::N_GETUNLIM = 5;
+
+std::vector <char*> data_usb(2),check(2);
+char* ACTION::getlineunlim()                                 //функция считывает строку до \n
+{
+    char *p;
+    int i = N_GETUNLIM, k = 0;
+    if (!(p = (char*) malloc(i * sizeof(char)))) goto fail;
+    fgets(&p[k], N_GETUNLIM, stdin);
+    k = strlen(p);
+    while (p[k-1]!='\n')
+    {
+        if ((k+N_GETUNLIM)>i)
+        {
+            i *= 2;
+            if (!(p = (char*) realloc(p, i * sizeof(char)))) goto fail;
+        }
+        fgets(&p[k], N_GETUNLIM, stdin);
+        k = strlen(p);
+    }
+    return p;
+    fail:
+        p = NULL;
+        return p;
+}
+
+int ACTION::count_USB(){                                     //считает количество воткнутых флешек, ее же надо вставить в демон для проверки
+    int pid1,pid2, fd1[2],fd2[2],numb = 0;
+    pipe(fd1);
+    pid1 = fork();
+    if(!pid1){
+        pipe(fd2);
+        pid2 = fork();
+        if(!pid2){
+            dup2(fd2[1],1);
+            close(fd1[1]);
+            close(fd1[0]);
+            close(fd2[0]);
+            close(fd2[1]);
+            execlp("blkid","blkid","-t","TYPE=vfat",NULL);
+            return -1;
+        }
+        else {
+        dup2(fd1[1],1);
+        dup2(fd2[0],0);
+        close(fd1[1]);
+        close(fd1[0]);
+        close(fd2[0]);
+        close(fd2[1]);
+        execlp("wc","wc","-l" ,NULL);
+        return -1;
+        }
+    }
+    else {
+        wait(NULL);
+        dup2(fd1[0],0);
+        close(fd1[0]);
+        close(fd1[1]);
+        char * p;
+        p = getlineunlim();
+        int i,k;
+        k = strlen(p);
+        numb = 0;
+        for (i = 0; i < k-1; ++i) numb = numb*10+p[i]- '0';
+    }
+    return numb;
+}
+void ACTION::takeusbinf(){                 //узнает информацию о воткнутой флешке, записывает ее в вектор data: 0)LABEL 1)UUID
+    int pid,fd[2];
+    pipe(fd);
+    pid = fork();
+    if(!pid){
+         dup2(fd[1],1);
+         close(fd[0]);
+         close(fd[1]);
+         execlp("blkid","blkid","-t" ,"TYPE=vfat",NULL);
+    }
+    else {
+         dup2(fd[0],0);
+         close(fd[0]);
+         close(fd[1]);
+         char * p,*q;
+         int z,t,k,i = 0;
+         p = getlineunlim();
+         k = strlen(p);
+         while(p[i++]!=' ');
+         for (z = 0; z < 2; ++z){
+            while(p[i++]!='"');
+            t = i;
+            q = new char[k];
+            while(p[i]!='"'){
+                q[i-t] = p[i];
+                i++;
+            }
+            q[i-t] = '\0';
+            i++;
+            if (z == 0) strcpy(token.label,q);
+            else strcpy(token.UID,q);
+         }
+         delete []q;
+    }
+}
+
